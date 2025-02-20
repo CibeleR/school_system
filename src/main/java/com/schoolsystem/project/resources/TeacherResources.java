4735b51e5ca83a3c6040e8ed9482a8d006978296
CibeleR <cibele.rodrigues17@hotmail.com>
2025-02-20 20:50:01 -0300

    CRUD da classe Teacher

--- src/main/java/com/schoolsystem/project/resources/TeacherResources.java
+++ src/main/java/com/schoolsystem/project/resources/TeacherResources.java
@@ -1,13 +1,20 @@
 package com.schoolsystem.project.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.schoolsystem.project.domain.Teacher;
 import com.schoolsystem.project.dto.TeacherDTO;
@@ -25,5 +32,32 @@ public class TeacherResources {
 		List<Teacher> list = service.findAll();
 		List<TeacherDTO> listDto = list.stream().map(x -> new TeacherDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<TeacherDTO> findById(@PathVariable String id){
+		Teacher obj = service.findById(id);
+		return ResponseEntity.ok().body(new TeacherDTO(obj));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Teacher teacher){
+		Teacher obj = service.insert(teacher);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody Teacher teacher, @PathVariable String id){
+		Teacher obj = service.insert(teacher);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/schoolsystem/project/services/TeacherService.java
+++ src/main/java/com/schoolsystem/project/services/TeacherService.java
@@ -1,12 +1,14 @@
 package com.schoolsystem.project.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.schoolsystem.project.domain.Teacher;
 import com.schoolsystem.project.repository.TeacherRepository;
+import com.schoolsystem.project.services.exception.ObjectNotFoundException;
 
 @Service
 public class TeacherService {
@@ -16,5 +18,30 @@ public class TeacherService {
 	
 	public List<Teacher> findAll(){
 		return repository.findAll();
+	}
+	
+	public Teacher findById(String id){
+		Optional<Teacher> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public Teacher insert(Teacher obj){
+		return repository.insert(obj);
+	}
+	
+	public void delete(String id) {
+		repository.deleteById(id);
+	}
+	
+	public Teacher update(Teacher obj) {
+		Teacher newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(Teacher newObj, Teacher obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+		newObj.setMatéria(obj.getMatéria());
 	}
 }
